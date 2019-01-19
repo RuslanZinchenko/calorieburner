ce92268539a2eb1bdd08a1e080b08cf8684c0bcc
Anya Efimova <gani4ka_n@ukr.net>
2019-01-19 17:59:35 +0200

    progress library added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./scss/styles.scss";
 import "@babel/polyfill";
+import "./loading-bar/loading-bar";
 // import View from "./js/view";
 // import Model from "./js/model";
 // import Controller from "./js/controller";
-
